fd0de8eda95295350a0f0f9b7dc41c9c9a5e0e99
Mike Berger <mikemanuelberger@gmail.com>
2026-02-27 08:24:12 +0100

    Use AddLumeo() in docs app to register all services

--- docs/Lumeo.Docs/Program.cs
+++ docs/Lumeo.Docs/Program.cs
@@ -1,14 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Lumeo;
 using Lumeo.Docs;
-using Lumeo.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ComponentInteropService>();
-builder.Services.AddScoped<ToastService>();
+builder.Services.AddLumeo();
 
 await builder.Build().RunAsync();
